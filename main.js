7238c66e5ee00f51912a980f443e3945d1d7b7ce
Todor Pavlov <pavlovt@gmail.com>
2019-02-26 20:23:14 +0200

    update

--- main.js
+++ main.js
@@ -7,6 +7,24 @@ function init() {
         e.preventDefault();
         // const url = $(this).data('url');
         // $('main').load(url);
-        $('main').html($('#products').html())
+        var name = $(this).data().name;
+        $('main').html($('#' + name).html());
+
+        // get the product template as text
+        let tpl = $('#product').html(), tpl1;
+        // get the products
+        $.get('http://localhost:3000/products')
+        .then((products) => {
+            console.log(products);
+            products.forEach((product) => {
+                // wrap it in div and create jQuery element of it
+                tpl1 = $('<div>'+tpl+'</div>')
+                tpl1.find('h3').html(product.name);
+                tpl1.find('.desc').html(product.name + ' description');
+                console.log(product.name, tpl1.html())
+                $('main .product-list').append(tpl1);
+            })
+
+        })
     })
 }
